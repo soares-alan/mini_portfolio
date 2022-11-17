766e40061d216b9e4428eab7d151a1c91e633a49
Alan Soares <86978240+soares-alan@users.noreply.github.com>
2022-11-16 22:16:10 -0300

    add update
    css att and  js index

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+/*
+Objetivo - mostrar o conteúdo da aba selecionada, escondendo a anterior.
+*/
+
+/* 1 - busca da classe aba */
+const abas = document.querySelectorAll(".aba");
+
+/* 2- indentificando click no elmento aba - 1:12min */
+abas.forEach((aba) => {
+    console.log(aba)
+});
